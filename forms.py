cbba653122e5452570362539a78140c6016fee61
xericho <xeric.hox@gmail.com>
2018-08-15 20:35:35 -0700

    removed useless code

--- forms.py
+++
@@ -1,8 +0,0 @@
-from wtforms import Form, StringField, SelectField
- 
-class MusicSearchForm(Form):
-    choices = [('title', 'Title'),
-               ('genre', 'Genre'),
-               ('duration', 'Duration')]
-    select = SelectField('Search for music:', choices=choices)
-    search = StringField('')
