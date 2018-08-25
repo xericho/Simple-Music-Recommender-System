fc238d93b0c61dce92f0e646c44447c5f8751a33
xericho <xeric.hox@gmail.com>
2018-08-25 00:26:36 -0700

    added item sim functionality

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-# from setup_flask import app
 from flask import Flask, flash, render_template, request, redirect, session, url_for
 from flask_table import Table, Col
-# import sqlite3 as sql
 import pickle
 import pandas as pd 
-import Recommenders
-
-# DATABASE = './mymusic.db'
-# DATABASE = './mymusicsample_v3.db'
+import Recommenders as R
 
 app = Flask(__name__)
 app.secret_key = 'secret'   # needed to flash notifications
-# def get_db():
-#     db = getattr(g, '_database', None)
-#     if db is None:
-#         db = g._database = sql.connect(DATABASE)
-#     return db
 
 def load_data(filename):
     with open(filename, 'rb') as f:
@@ -31,45 +21,12 @@ def dashboard():
     user_id = None
     if request.method == "POST":
         user_id = request.form['user_id']
-    return render_template('dashboard.html', df_clean=data_clean, pm=pop_rec, df=data, user_id=user_id)
-
-@app.route('/signin/')
-def signin():    
-    return render_template('signin.html')
-
-@app.route("/songs/", methods=['GET', 'POST'])
-# def songs():
-#     with app.app_context():      # for auto closing when out of scope
-#         db = get_db()
-#         cursor = db.execute("SELECT title, genre, duration, url_link FROM music LIMIT 50") # table is called musics
-#         items = cursor.fetchall()
-#         return render_template('songs.html', items=items)
-def songs():
-    return render_template('songs.html', df=data_clean)
-
-@app.route('/history/')
-def history():    
-    return render_template('history.html')
-
-@app.route('/recommend/', methods=['GET','POST'])
-def recommend():    
-    return render_template('recommend.html', pm=pop_rec, df=data)
-
-@app.route('/about/')
-def about():    
-    return render_template('about.html')
-
-@app.route('/update_df/')
-def update_df():
-    print("updated!")
-    return 'nothing'
-
-""" For closing database """
-# @app.teardown_appcontext
-# def close_connection(exception):
-#     db = getattr(g, '_database', None)
-#     if db is not None:
-#         db.close()
+    return render_template('dashboard.html', 
+                           df_clean=data_clean, 
+                           pop_model=pop_rec, 
+                           df=data, 
+                           user_id=user_id, 
+                           is_model=is_model)
 
 # run the application
 if __name__ == "__main__":  
@@ -78,10 +35,14 @@ if __name__ == "__main__":
     data_clean = data.drop_duplicates(subset='title')
 
     # Create an instance of popularity based recommender class
-    pm = Recommenders.popularity_recommender_py()
-    pm.create(data, 'user_id', 'title')
-    pop_rec = pm.recommend(data.iloc[0]['user_id'])     # recommend only once
+    pop_model = R.popularity_recommender_py()
+    pop_model.create(data, 'user_id', 'title')
+    pop_rec = pop_model.recommend(data.iloc[0]['user_id'])     # recommend only once
 
+    # Initialize class for item similarity model
+    is_model = R.item_similarity_recommender_py() 
+    is_model.create(data, 'user_id', 'title')
+    
     app.run(debug=True)
 
 
